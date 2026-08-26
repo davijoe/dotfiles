f9e02f2e83d673054ec70005dc7dacb8e29c611c
davijoe <dvj@magenta-aps.dk>
2026-08-26 10:47:56 +0200

    Feature: mkcd function. Some addr

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -2,3 +2,6 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 export PATH="$HOME/.local/bin:$PATH"
+
+# The next line updates PATH for the Google Cloud SDK.
+if [ -f '/home/dan/Downloads/google-cloud-sdk/path.fish.inc' ]; . '/home/dan/Downloads/google-cloud-sdk/path.fish.inc'; end
