e422853c2ebda631b448da269c3c5bfdf1f73d32
davijoe <dvj@magenta-aps.dk>
2026-09-03 10:23:47 +0200

    Better abbrs

--- fish/.config/fish/conf.d/abbrs.fish
+++ fish/.config/fish/conf.d/abbrs.fish
@@ -1,17 +1,22 @@
-abbr -a -- gs 'git status'
 abbr -a -- ld lazydocker
 abbr -a -- lg lazygit
+abbr -a -- . 'nvim .'
+
+abbr -a -- gs 'git status'
+abbr -a -- gl 'git log --oneline --graph --decorate'
 abbr -a -- gcm 'git commit -m'
 abbr -a -- gaa 'git add --all'
-abbr -a -- . 'nvim .'
-abbr -a -- dps 'docker ps'
-abbr -a -- dka 'cd /home/dan/App/kadk-nextcloud'
-abbr -a -- hypr 'cd /home/dan/.config/hypr/'
-abbr -a -- conf 'cd /home/dan/.config/'
+abbr -a -- dps 'sudo docker ps'
+
+abbr -a -- dka 'cd $HOME/App/kadk-nextcloud'
+abbr -a -- hypr 'cd $HOME/.config/hypr/'
+abbr -a -- conf 'cd $HOME/.config/'
+
 abbr -a -- ... 'cd ../../'
 abbr -a -- .... 'cd ../../../'
 abbr -a -- ..... 'cd ../../../../'
+
 abbr -a -- down 'just down'
 abbr -a -- up 'just devenv-deploy'
 abbr -a -- shokz 'bluetoothctl connect A0:0C:E2:D6:25:A5'
-abbr -a -- gl 'git log --oneline --graph --decorate'
+abbr -a -- saveabbr 'abbr > ~/dotfiles/fish/.config/fish/conf.d/abbrs.fish'
